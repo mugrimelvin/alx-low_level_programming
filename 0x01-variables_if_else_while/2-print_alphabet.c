d5752c633aa8b61f3beaa8a9d71bc9936d3c3474
mugrimelvin <mugrimelvin@hotmail.com>
2023-02-17 15:40:57 -0800

    Var-if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entrée
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	for (a = 'a'; a <= 'z'; a++)
+        {
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
