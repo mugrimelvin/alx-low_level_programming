dc21c2c93bb324a261337fb34e1be47b92ae8d4f
mugrimelvin <mugrimelvin@hotmail.com>
2023-02-17 16:07:35 -0800

    Var-if_else_while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entrée
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+        int a = '0';
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
