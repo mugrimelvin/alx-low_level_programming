ceba9aea18e3cf07074ff2da31c943ca510b4da8
mugrimelvin <mugrimelvin@hotmail.com>
2023-02-21 18:31:43 -0800

    Func_Nested_loop

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-        int a, b;
-
-        for (a = 1; a < 1024; a++)
-        {
-                if ((a % 3) == 0 || (a % 5) == 0)
-                        b += a;
-        }
-        printf("%d\n", b);
-        return (0);
+	int a, b;
+	
+	for (a = 1; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
+	}
+	printf("%d\n", b);
+	return (0);
 }
