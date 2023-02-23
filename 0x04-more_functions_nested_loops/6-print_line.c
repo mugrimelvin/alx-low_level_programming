3f6fbf6f537f8d745914e59ea5e8709d93c74159
mugrimelvin <mugrimelvin@hotmail.com>
2023-02-23 12:26:01 -0800

    More_Func_More_Nest

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,27 +1,19 @@
 #include "main.h"
 
 /**
-  * print_line - Draws a straight line according to parameter
+  * print_line - Draws a straight line
   * @n: The number of lines to draw
   *
   * Return: empty
   */
 void print_line(int n)
 {
-	int x;
+	int i;
 
-	if (n <= 0)
+	if (i = 0; i < n; i++)
 	{
-		_putchar('\n');
+		_putchar(95);
 	}
-	else
-	{
-		for (x = 0; x < n; x++)
-		{
-		       _putchar(95);
-		}
-
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
 
