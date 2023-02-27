0fd37a9f29eac7f91911853f781a788653509184
mugrimelvin <mugrimelvin@hotmail.com>
2023-02-27 15:23:46 -0800

    Points_Arrays_Strings

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,17 +10,13 @@
   */
 void print_array(int *a, int n)
 {
-	int j
-	
-	for (j = 0; j < n; j++)
-	{
-		printf("%d", a[j]);
-		
-		if (j != (n - 1))
-		{
-			printf(", ");
-		}
-	}
+	int i = 0;
 
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
 	printf("\n");
 }
