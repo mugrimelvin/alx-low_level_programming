4873a1401cca852e2909d0a13275466e348eeb34
mugrimelvin <mugrimelvin@hotmail.com>
2023-03-01 12:36:29 -0800

    x06-Points_Arrays_Strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -4,24 +4,24 @@
 /**
  * print_buffer - prints a buffer
  * @b: buffer.
- * @size: size of buffer.
+g* @size: size of buffer.
  * Return: no return.
  */
 void print_buffer(char *b, int size)
 {
-        int j, k, l;
+	int j, k, l;
 
-        if (size <= 0)
-                printf("\n");
-        else
-        {
-                for (j = 0; j < size; j += 10)
-                {
-                        printf("%.8x:", j);
-                        for (k = j; k < j + 10; k++)
-                        {
-                                if (k % 2 == 0)
-                                        printf(" ");
+	if (size <= 0)
+		printf("\n");
+	else
+	{
+		for (j = 0; j < size; j += 10)
+		{
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
+			{
+				if (k % 2 == 0)
+					printf(" ");
                                 if (k < size)
                                         printf("%.2x", *(b + k));
                                 else
