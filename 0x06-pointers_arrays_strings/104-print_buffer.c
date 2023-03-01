b5b7a93d22b8a84cff3a7cd8dcb68d856067e361
mugrimelvin <mugrimelvin@hotmail.com>
2023-03-01 12:45:50 -0800

    x06-Points_Arrays_Strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -22,23 +22,23 @@ void print_buffer(char *b, int size)
 			{
 				if (k % 2 == 0)
 					printf(" ");
-                                if (k < size)
-                                        printf("%.2x", *(b + k));
-                                else
-                                        printf("  ");
-                        }
-                        printf(" ");
-                        for (l = j; l < j + 10; l++)
-                        {
-                                if (l >= size)
-                                        break;
-                                if (*(b + l) < 32 || *(b + l) > 126)
-                                        printf("%c", '.');
-                                else
-                                        printf("%c", *(b + l));
-                        }
-                        printf("\n");
-                }
-        }
+				if (k < size)
+					printf("%.2x", *(b + k));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (l = j; l < j + 10; l++)
+			{
+				if (l >= size)
+					break;
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + l));
+			}
+			printf("\n");
+		}
+	}
 }
 
