4a84ec1eb0547391b69a059eaae3939c00e8a362
mugrimelvin <mugrimelvin@hotmail.com>
2023-03-08 15:29:22 -0800

    Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -14,6 +14,6 @@ void _puts_recursion(char *s)
 	}
 
 	_putchar(*s);
-	s++
+	s++;
 	_puts_recursion(s);
 }
--- 0x08-recursion/main2.h
+++ 0x08-recursion/main2.h
@@ -1,7 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -13,3 +12,4 @@ int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
 #endif
+~
