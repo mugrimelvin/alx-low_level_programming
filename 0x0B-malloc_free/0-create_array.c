57426c9327299e839c6ceedaa2ed1a135a0f9816
mugrimelvin <mugrimelvin@hotmail.com>
2023-03-14 18:02:48 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,20 +9,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *cr;
-	unsigned int i
+char *str;
+		
+	unsigned int i;
+	
+	str = malloc(sizeof(char) * size);
 
-	if (size == 0)
-		return (NULL);
-
-	cr = malloc(sizeof(c) * size);
-
-	if (cr == NULL)
+	if (size == 0 || str == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		cr[i] = c;
-
-	return (cr);
+		str[i] = c;
+		
+	return (str);
+		
 }
 
